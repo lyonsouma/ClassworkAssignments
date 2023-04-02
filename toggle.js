b10fe52244dec106c5c6f48a286665a3ac669e7c
Lyons Ouma <oumalyons26@gmail.com>
2023-04-02 20:23:58 +0300

    Toggle manu

---
+++ toggle.js
@@ -0,0 +1,39 @@
+let toggleNavStatus = false;
+
+let toggleNav = function(){
+   let getSidebar = document.querySelector("#nav-aside")
+   let getSidebarUl = document.querySelector("#nav-aside ul")
+   let getSidebarTitle = document.querySelector("#nav-aside span")
+   let getSidebarLinks = document.querySelectorAll("#nav-aside a")
+
+   if(toggleNavStatus === false) {
+    getSidebarUl.style.visibility = "visible";
+    getSidebar.style.width = "272px";
+    getSidebarTitle.style.opacity = "0.5";
+
+
+    let arrayLength = getSidebarLinks.length;
+    for (let i = 0; i < arrayLength; i ++) {
+        getSidebarLinks[i].style.opacity = "1";
+        
+    }
+
+    toggleNavStatus = true;
+   }
+
+   
+   else if(toggleNavStatus === true) {
+    getSidebarUl.style.visibility = "hidden";
+    getSidebar.style.width = "50px";
+    getSidebarTitle.style.opacity = "0";
+
+
+    let arrayLength = getSidebarLinks.length;
+    for (let i = 0; i < arrayLength; i ++) {
+        getSidebarLinks[i].style.opacity = "0";
+        
+    }
+
+    toggleNavStatus = true;
+   }
+}
