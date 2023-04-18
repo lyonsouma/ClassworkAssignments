906db67f829a0dd49bf6e9bd7bcbedb5097dd6ea
Lyons Ouma <oumalyons26@gmail.com>
2023-04-18 12:59:33 +0300

    Abstract in js

--- classes.js
+++ classes.js
@@ -1,3 +1,5 @@
+                    // INHERITANCE
+ /*
  class User{
     constructor(name, sex, age){
         this.name = name;
@@ -21,3 +23,98 @@
 
  User2.login();
  User3.logout();
+ */
+
+   /*                         // ASSIGNMENT(17/4/2023)
+    
+    class Person {
+        constructor(name, age, gender){
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+        }
+       eat(){
+        console.log(this.name, 'can eat');
+       }
+
+       walk(){
+         console.log(this.name, 'can walk');
+       }
+    }
+
+    class Student extends Person{
+        constructor(name, age, gender,program, studyYear){
+          super(name, age, gender)
+            this.program = program;
+            this.studyYear = studyYear;
+        }
+       study(){
+        console.log(this.program, 'was studied');
+       }
+
+       hadExam(){
+         console.log(this.name, 'took exam');
+       }
+    }
+
+
+
+    class Teacher extends Person{
+        constructor(name, age, gender,designation, salary){
+          super(name, age, gender)
+            this.designation = designation;
+            this.salary = salary;
+        }
+       teach(){
+        console.log(this.name, 'can teach');
+       }
+
+       takeExam(){
+         console.log(this.name, 'can sit for an examination');
+       }        
+    }
+
+
+
+    class Doctor extends Person{
+        constructor(name, age, gender,designation,salary){
+          super(name, age, gender)
+            this.designation = designation;
+            this.salary = salary;
+        }
+       checkup(){
+        console.log(this.name, 'is responsible for checkup since he is a docter');
+       }
+
+       prescribe(){
+         console.log(this.name, 'gave prescription');
+       }       
+    }
+
+    var doc = new Person('Lyons', 21 , 'male', 'CEO' , 300000)
+    console.log(doc);
+    */
+
+                  // ABSTRACTION
+   class Employee{
+    #monthlyBonus;
+    constructor(name, age, salary){
+      this.name = name;
+      this.age = age;
+      this.salary = salary;
+      this.#monthlyBonus = 20000; 
+    }
+
+    finalSalary(){
+      let FinalSalary = this.salary + this.#monthlyBonus;
+      console.log("Your final salary is: " + FinalSalary);
+    }
+
+    get getEmpDetails(){
+      // return(`Your name is: ${this.name} , You are :${this.age} years old, your monthly salary is ${this.salary}`);
+      return this.finalSalary();
+    }
+   } 
+
+   Emp1 = new Employee('John', 21 , 450000);
+   console.log(Emp1);
